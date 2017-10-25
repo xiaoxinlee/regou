e45ab9e60093561ba0f2a3c13b210735325facea
xiaoxinlee <1103909160@qq.com>
2017-10-25 16:59:07 +0800

    1025

---
+++ static/js/news.js
@@ -0,0 +1,330 @@
+
+var aip=   {
+	"result":{
+		"stat":"1",
+		"data":[
+			{
+				"uniquekey":"49532bb7f3c0131e7ce369a85b44d875",
+				"title":"这些山寨货让全世界都笑死了！简直坑爹啊！",
+				"date":"2017-10-22 13:02",
+				"category":"社会",
+				"author_name":"看什么鬼",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022130246948.html",
+				"thumbnail_pic_s":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_611969d6b4ec3114b0f29dfa57948b29_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_3cae10a352cd0127a90a303083714863_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_7d15754dc1efb2f729f323b0d120e435_cover_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"914eb9d99b2ec5f9c2e07ffb72e02079",
+				"title":"强壮大妈车上撕扯小女孩衣服，旁边人却无动于衷，结果让人愤怒",
+				"date":"2017-10-22 13:01",
+				"category":"社会",
+				"author_name":"爱生活爱探秘",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022130146687.html",
+				"thumbnail_pic_s":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022_0b6886d97b4d76bb58afbe2ca13d20d5_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022_d71a59d0d12184eb96cc502760da961c_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022_f993dbe96f66b15946255ac5a33d6b27_cover_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"4b6e1186a9f8dfaaa6ad895a40cda367",
+				"title":"敬一丹携作品与呼和浩特读者见面",
+				"date":"2017-10-22 13:00",
+				"category":"社会",
+				"author_name":"呼和浩特日报",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022130020168.html",
+				"thumbnail_pic_s":"http:\/\/01.imgmini.eastday.com\/mobile\/20171022\/20171022130020_1889cbc0e37fbc244ef654e4309fc850_1_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"dec3bdc28710039972e41864a4ffb1d4",
+				"title":"四川又要新添一座新机场，这个地市要腾飞了！",
+				"date":"2017-10-22 12:55",
+				"category":"社会",
+				"author_name":"水流沙坝",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022125518779.html",
+				"thumbnail_pic_s":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022125518_1f7379c00259b066f5ad2c7dbd01d86d_1_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022125518_1f7379c00259b066f5ad2c7dbd01d86d_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022125518_1f7379c00259b066f5ad2c7dbd01d86d_3_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"2edd537fdf1d9a37fae5c900d32c009b",
+				"title":"跟党走，国家好，我们就好！",
+				"date":"2017-10-22 12:54",
+				"category":"社会",
+				"author_name":"中国文明网",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022125459792.html",
+				"thumbnail_pic_s":"http:\/\/06.imgmini.eastday.com\/mobile\/20171022\/20171022125459_a730c054dddb41ae79b06d7a5ee7110d_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/06.imgmini.eastday.com\/mobile\/20171022\/20171022125459_a730c054dddb41ae79b06d7a5ee7110d_1_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"5e05f455cbb3807b53fb3858c57b2863",
+				"title":"“宋庄十年 感恩同行”康漠个人油画作品展在北京开幕",
+				"date":"2017-10-22 12:53",
+				"category":"社会",
+				"author_name":"搜狐",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022125315876.html",
+				"thumbnail_pic_s":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022125315_5ed91d0f55cb2e78c7edc434f974d7fb_8_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022125315_5ed91d0f55cb2e78c7edc434f974d7fb_10_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022125315_5ed91d0f55cb2e78c7edc434f974d7fb_1_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"43b2b8e819c2931737f614ccc34ec4a4",
+				"title":"男子走私大批珍奇动物，幸得警方及时将其拦截救下了这些小生命",
+				"date":"2017-10-22 12:52",
+				"category":"社会",
+				"author_name":"小情绪的世界",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022125223761.html",
+				"thumbnail_pic_s":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022125223_d5c2616688ae9164d4efdb2387cf6b7e_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022125223_d5c2616688ae9164d4efdb2387cf6b7e_3_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022125223_d5c2616688ae9164d4efdb2387cf6b7e_4_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"1eb0895b4ba7bd3f1fc9a23fdf021b36",
+				"title":"王雪涛花鸟画专辑公鸡",
+				"date":"2017-10-22 12:46",
+				"category":"社会",
+				"author_name":"童话镇绘鑫",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022124657728.html",
+				"thumbnail_pic_s":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022124657_d40ae20b6bcbb8909287a5ae644a94cb_5_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022124657_d40ae20b6bcbb8909287a5ae644a94cb_27_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022124657_d40ae20b6bcbb8909287a5ae644a94cb_4_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"5f13ab78a1cdaf95e028e66094ec06b8",
+				"title":"南京六合:村民公路上压稻谷导致两轿车追尾",
+				"date":"2017-10-22 12:46",
+				"category":"社会",
+				"author_name":"龙虎网",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022124648018.html",
+				"thumbnail_pic_s":"http:\/\/00.imgmini.eastday.com\/mobile\/20171022\/20171022124648_5aecd9b4b3766c955b548df6f7060019_1_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"31a62b094a5572b8f4d3fd229bdd2554",
+				"title":"浙江美好生活的变化在哪里？请看一名记者的感悟",
+				"date":"2017-10-22 12:46",
+				"category":"社会",
+				"author_name":"温州网",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022124621910.html",
+				"thumbnail_pic_s":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022124621_870249302d405c338a10caa471fd236e_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022124621_870249302d405c338a10caa471fd236e_1_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022124621_870249302d405c338a10caa471fd236e_3_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"150481a1e70353e45ddf373515526022",
+				"title":"他用“饮料”来制作“水煮蛋”，结果出来的成品让人不可思议",
+				"date":"2017-10-22 12:42",
+				"category":"社会",
+				"author_name":"爱生活爱探秘",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022124200446.html",
+				"thumbnail_pic_s":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022_8a320f9d272ed45ce588de571754b38d_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022_c4943d466aa642807e7687cfa8fa2313_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/09.imgmini.eastday.com\/mobile\/20171022\/20171022_d66d98573b5b6c3899d08abb5f93a7a6_cover_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"47b20d77f4413ae85c4e737dba2d6510",
+				"title":"没有违章也可能被罚被拘！原因是你没检查这个...",
+				"date":"2017-10-22 12:39",
+				"category":"社会",
+				"author_name":"佛山电台飞跃924",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022123922853.html",
+				"thumbnail_pic_s":"http:\/\/08.imgmini.eastday.com\/mobile\/20171022\/20171022123922_00fe79039ddb95bb6ff992537f490d06_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/08.imgmini.eastday.com\/mobile\/20171022\/20171022123922_00fe79039ddb95bb6ff992537f490d06_4_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/08.imgmini.eastday.com\/mobile\/20171022\/20171022123922_00fe79039ddb95bb6ff992537f490d06_3_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"dfed9f4d18cce5f897a8490fea2ea20b",
+				"title":"千人计划海归教授因病早逝，不住别墅住办公室，被学生授予“交大黄大年”、废寝忘食奖",
+				"date":"2017-10-22 12:36",
+				"category":"社会",
+				"author_name":"解放网",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022123649409.html",
+				"thumbnail_pic_s":"http:\/\/01.imgmini.eastday.com\/mobile\/20171022\/20171022123649_9640b8d518e9b36aaba28fb51c4d732a_5_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/01.imgmini.eastday.com\/mobile\/20171022\/20171022123649_9640b8d518e9b36aaba28fb51c4d732a_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/01.imgmini.eastday.com\/mobile\/20171022\/20171022123649_9640b8d518e9b36aaba28fb51c4d732a_4_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"5b00159619e2809adbb3f29b7ebd29af",
+				"title":"小伙河边钓到大鱼，将其放生后，众人却说他假好心",
+				"date":"2017-10-22 12:33",
+				"category":"社会",
+				"author_name":"学生圈",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022123314559.html",
+				"thumbnail_pic_s":"http:\/\/05.imgmini.eastday.com\/mobile\/20171022\/20171022123314_f666c0aac4dadbecf4483b75bbff6ac9_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/05.imgmini.eastday.com\/mobile\/20171022\/20171022123314_f666c0aac4dadbecf4483b75bbff6ac9_4_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/05.imgmini.eastday.com\/mobile\/20171022\/20171022123314_f666c0aac4dadbecf4483b75bbff6ac9_3_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"a533bfde97f4c18341da7f988b347a19",
+				"title":"200平露台花园打造中，露台太大也挺苦恼的",
+				"date":"2017-10-22 12:31",
+				"category":"社会",
+				"author_name":"阳台设计精选",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022123158410.html",
+				"thumbnail_pic_s":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022123158_c613e2be33be115723dda25c92c9a371_22_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022123158_c613e2be33be115723dda25c92c9a371_13_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022123158_c613e2be33be115723dda25c92c9a371_25_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"8f8fab923f95e15b2066876fd8810270",
+				"title":"叛逆少女离家出走，上海警方通过共享位置定位将其寻获",
+				"date":"2017-10-22 12:31",
+				"category":"社会",
+				"author_name":"澎湃新闻网",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022123156302.html",
+				"thumbnail_pic_s":"http:\/\/05.imgmini.eastday.com\/mobile\/20171022\/20171022123156_d41d8cd98f00b204e9800998ecf8427e_1_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"eeb18bcd14fce75489fa58e1e8aafa2d",
+				"title":"鄂州警方威武！凌晨围歼特大盗窃团伙，嫌疑人被按倒！",
+				"date":"2017-10-22 12:31",
+				"category":"社会",
+				"author_name":"鄂州在线",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022123134753.html",
+				"thumbnail_pic_s":"http:\/\/00.imgmini.eastday.com\/mobile\/20171022\/20171022123134_16f8f28b779f1e7e17b67119eeac1180_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/00.imgmini.eastday.com\/mobile\/20171022\/20171022123134_16f8f28b779f1e7e17b67119eeac1180_3_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/00.imgmini.eastday.com\/mobile\/20171022\/20171022123134_16f8f28b779f1e7e17b67119eeac1180_1_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"872168ec6a8b8ecf2d055b520b2ea9bf",
+				"title":"说出来你可能不信，豪车的尴尬之无处安放的车牌",
+				"date":"2017-10-22 12:28",
+				"category":"社会",
+				"author_name":"探索奇闻",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122835733.html",
+				"thumbnail_pic_s":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_13308264a6d2d07ffca6c69318c58d6c_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_fa7d7584684544f163f3cf6ca9e73b73_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_00de949e0674a151205e2a0c9755455e_cover_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"67db08d2b28481100b382e4efaaace1f",
+				"title":"彭泽：跟着棉船爱心协会走进社区 看这些好人温暖贫困居民心",
+				"date":"2017-10-22 12:28",
+				"category":"社会",
+				"author_name":"搜狐",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122823862.html",
+				"thumbnail_pic_s":"http:\/\/06.imgmini.eastday.com\/mobile\/20171022\/20171022122823_86a119b2cab08903d78eb03555c759b0_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/06.imgmini.eastday.com\/mobile\/20171022\/20171022122823_86a119b2cab08903d78eb03555c759b0_1_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/06.imgmini.eastday.com\/mobile\/20171022\/20171022122823_86a119b2cab08903d78eb03555c759b0_3_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"502efda248d08b0c12b9fe2e1456c2d9",
+				"title":"教你识破潮州街头的这一骗局，别让你的爱心被猥亵",
+				"date":"2017-10-22 12:28",
+				"category":"社会",
+				"author_name":"搜狐",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122821392.html",
+				"thumbnail_pic_s":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022122821_ee2670574cd4ed4fb87fdb31d4a7dba6_4_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022122821_ee2670574cd4ed4fb87fdb31d4a7dba6_13_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022122821_ee2670574cd4ed4fb87fdb31d4a7dba6_2_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"f8e568b902db081208f9c9fd79adad89",
+				"title":"光鲜亮丽的外表下，暗藏危机，你所不知道的保护色",
+				"date":"2017-10-22 12:28",
+				"category":"社会",
+				"author_name":"探索奇闻",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122810959.html",
+				"thumbnail_pic_s":"http:\/\/04.imgmini.eastday.com\/mobile\/20171022\/20171022_655cc8ceec616c5eeaa9729ebc1adeec_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/04.imgmini.eastday.com\/mobile\/20171022\/20171022_5a1fc265fb77e5f79b15cde1f038a266_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/04.imgmini.eastday.com\/mobile\/20171022\/20171022_c2cd6c5b65ff702c9443f5a3a1dd5ea0_cover_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"7a8b0fc8072bd5f3e0e53701aa0bc086",
+				"title":"两个头的鲨鱼，原因究竟是为了什么？没有买卖就没有杀害",
+				"date":"2017-10-22 12:27",
+				"category":"社会",
+				"author_name":"探索奇闻",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122729624.html",
+				"thumbnail_pic_s":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_6811952467dd0e8a1f836b4f9da53682_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_a29df59250702db092b21ba64f5068fa_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022_1db5eda45de46afa9a8ab5f16cba7cf4_cover_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"f10838e11fb007914940018dae565a10",
+				"title":"男子心存侥幸醉驾车 民警火眼金睛识“醉汉”",
+				"date":"2017-10-22 12:26",
+				"category":"社会",
+				"author_name":"南京交警",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122614826.html",
+				"thumbnail_pic_s":"http:\/\/02.imgmini.eastday.com\/mobile\/20171022\/20171022122614_7c15ab1418d8f8b26f48fb46e235d1a7_1_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"c159a0df7a2c9689cb3f71f53ff2e927",
+				"title":"60年代的吃肉记忆：凌晨排队买，肥肉最好卖，猪肉骨头要吃三次",
+				"date":"2017-10-22 12:25",
+				"category":"社会",
+				"author_name":"老梁观历史",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122523164.html",
+				"thumbnail_pic_s":"http:\/\/01.imgmini.eastday.com\/mobile\/20171022\/20171022122523_a5efb8ee5d07a90acf9e4da6eb780792_4_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/01.imgmini.eastday.com\/mobile\/20171022\/20171022122523_a5efb8ee5d07a90acf9e4da6eb780792_5_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/01.imgmini.eastday.com\/mobile\/20171022\/20171022122523_a5efb8ee5d07a90acf9e4da6eb780792_3_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"ddaa65486dd27fdbc42b154021d6204e",
+				"title":"母亲生活无法自理 19岁依兰姑娘背母上学成校园最美风景",
+				"date":"2017-10-22 12:22",
+				"category":"社会",
+				"author_name":"东北网",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122217680.html",
+				"thumbnail_pic_s":"http:\/\/07.imgmini.eastday.com\/mobile\/20171022\/20171022122217_f9bafb7b35cf5184f24077ff1d2edecb_1_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/07.imgmini.eastday.com\/mobile\/20171022\/20171022122217_e63ca0a48eeec26966fa1263eb261c8f_2_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"be02925d38a6c7122490bf894e640cd2",
+				"title":"在殡仪馆为死者洗澡的90后女孩，每天只睡4个小时，扛起整个家",
+				"date":"2017-10-22 12:22",
+				"category":"社会",
+				"author_name":"开阔视野",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122206864.html",
+				"thumbnail_pic_s":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022_6e0893f0eddc53e10fa0cb882d4e4089_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022_8f7c095aecf26927529411316054db4e_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/03.imgmini.eastday.com\/mobile\/20171022\/20171022_aad729b03bdf3e09eb6352333d2de32d_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"9d7777c4c52dc614ec6b5c849d982975",
+				"title":"许辉《人人都爱在水边》在合肥首发",
+				"date":"2017-10-22 12:21",
+				"category":"社会",
+				"author_name":"安徽网",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022122102489.html",
+				"thumbnail_pic_s":"http:\/\/01.imgmini.eastday.com\/mobile\/20171022\/20171022122102_40ad997814fec4d4c3c1fff9f13ed2f0_1_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"361d22425c80080acb04029c81705837",
+				"title":"名画竟然和你“撞衫”这是一种什么样的体验？",
+				"date":"2017-10-22 12:16",
+				"category":"社会",
+				"author_name":"搜狐",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022121610526.html",
+				"thumbnail_pic_s":"http:\/\/08.imgmini.eastday.com\/mobile\/20171022\/20171022121610_18896cea2ebfbcce6bfacb2b22e201e4_16_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/08.imgmini.eastday.com\/mobile\/20171022\/20171022121610_18896cea2ebfbcce6bfacb2b22e201e4_12_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/08.imgmini.eastday.com\/mobile\/20171022\/20171022121610_18896cea2ebfbcce6bfacb2b22e201e4_9_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"8c41fe90100d2807455e343785bce968",
+				"title":"中科院院士惊人建议：藏水入疆，西藏的水不再白白流向国外",
+				"date":"2017-10-22 12:16",
+				"category":"社会",
+				"author_name":"搜狐",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022121606807.html",
+				"thumbnail_pic_s":"http:\/\/05.imgmini.eastday.com\/mobile\/20171022\/20171022121606_754bc8eafab88f99a915dfc8c05afb51_2_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/05.imgmini.eastday.com\/mobile\/20171022\/20171022121606_754bc8eafab88f99a915dfc8c05afb51_1_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/05.imgmini.eastday.com\/mobile\/20171022\/20171022121606_754bc8eafab88f99a915dfc8c05afb51_3_mwpm_03200403.jpg"
+			},
+			{
+				"uniquekey":"d41364fadaaf966c03abb9ccb46951c4",
+				"title":"美国网友热议: 中国商场里的老公寄存处",
+				"date":"2017-10-22 12:15",
+				"category":"社会",
+				"author_name":"奇人囧事",
+				"url":"http:\/\/mini.eastday.com\/mobile\/171022121535847.html",
+				"thumbnail_pic_s":"http:\/\/04.imgmini.eastday.com\/mobile\/20171022\/20171022_8543c3bd7b83dd3753cf4a13313178c6_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s02":"http:\/\/04.imgmini.eastday.com\/mobile\/20171022\/20171022_bf75eb3f793ac9992837889086cb3113_cover_mwpm_03200403.jpg",
+				"thumbnail_pic_s03":"http:\/\/04.imgmini.eastday.com\/mobile\/20171022\/20171022_6c3a63e612196e75ab2137db209b3c19_cover_mwpm_03200403.jpg"
+			}
+		]
+	},
+	"error_code":0
+} 
+var obj = eval(aip);
+//alert(obj.constructor);
+//console.log(aip)
